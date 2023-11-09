9c74488259c1b44ab880854fb6aed5b269e70ad8
neonxflame <124432647+shermanTWQ@users.noreply.github.com>
2023-11-09 14:55:41 +0800

    Add files via upload

---
+++ FileHunter/FileHunter/FileHunter.Designer.vb
@@ -0,0 +1,87 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FileHunter
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFileName = New System.Windows.Forms.TextBox()
+        Me.lblFileName = New System.Windows.Forms.Label()
+        Me.btnSearch = New System.Windows.Forms.Button()
+        Me.lblDisplay = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtFileName
+        '
+        Me.txtFileName.Location = New System.Drawing.Point(38, 45)
+        Me.txtFileName.Name = "txtFileName"
+        Me.txtFileName.Size = New System.Drawing.Size(184, 22)
+        Me.txtFileName.TabIndex = 0
+        Me.txtFileName.Text = "<input file name>"
+        Me.txtFileName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblFileName
+        '
+        Me.lblFileName.AutoSize = True
+        Me.lblFileName.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblFileName.Location = New System.Drawing.Point(25, 25)
+        Me.lblFileName.Name = "lblFileName"
+        Me.lblFileName.Size = New System.Drawing.Size(214, 17)
+        Me.lblFileName.TabIndex = 1
+        Me.lblFileName.Text = "Please provide full file name"
+        '
+        'btnSearch
+        '
+        Me.btnSearch.Location = New System.Drawing.Point(38, 98)
+        Me.btnSearch.Name = "btnSearch"
+        Me.btnSearch.Size = New System.Drawing.Size(184, 23)
+        Me.btnSearch.TabIndex = 2
+        Me.btnSearch.Text = "Search for file!"
+        Me.btnSearch.UseVisualStyleBackColor = True
+        '
+        'lblDisplay
+        '
+        Me.lblDisplay.AutoSize = True
+        Me.lblDisplay.Location = New System.Drawing.Point(38, 74)
+        Me.lblDisplay.Name = "lblDisplay"
+        Me.lblDisplay.Size = New System.Drawing.Size(0, 17)
+        Me.lblDisplay.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 135)
+        Me.Controls.Add(Me.lblDisplay)
+        Me.Controls.Add(Me.btnSearch)
+        Me.Controls.Add(Me.lblFileName)
+        Me.Controls.Add(Me.txtFileName)
+        Me.Name = "Form1"
+        Me.Text = "FileHunter - 1.0"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFileName As TextBox
+    Friend WithEvents lblFileName As Label
+    Friend WithEvents btnSearch As Button
+    Friend WithEvents lblDisplay As Label
+End Class
---
+++ FileHunter/FileHunter/FileHunter.vb
@@ -0,0 +1,30 @@
+﻿Public Class FileHunter
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        'FileHunter 1.0
+        'Provided for non-commercial use. Please refer to Github Repo for more information. 
+        'Contact: Sherman_tan@outlook.com
+        '(C) Tan Wei Qiang Sherman.
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSearch.Click
+        Dim SourcePath As String = txtFileName.Text
+        Dim SaveDirectory As String = "C:\"
+
+        Try
+            Dim Filename As String = System.IO.Path.GetFileName(SourcePath)
+            Dim SavePath As String = System.IO.Path.Combine(SaveDirectory, Filename)
+            If System.IO.File.Exists(SavePath) Then
+                MsgBox("The file name " + txtFileName.Text + " was located in C: drive. The file is located at " + SavePath + ".")
+                lblDisplay.Text = "File is located!"
+            Else
+                MsgBox("The file name " + txtFileName.Text + " could not be located in C: drive.")
+                lblDisplay.Text = "File is not located!"
+            End If
+        Catch
+            lblDisplay.Text = "Illegal characters in textbox or file not found."
+        End Try
+
+    End Sub
+End Class
